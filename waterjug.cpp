80add6657e599354e0d0f890308e595019fa73d0
lhalcomb <lhalcomb@drury.edu>
2025-01-17 15:17:22 -0600

    comitting new files to start on

---
+++ waterjug.cpp
@@ -0,0 +1,11 @@
+/*
+2. Give a complete formulation (state, initial state, actions, etc.) to the following problem. 
+
+Choose a formulation that is precise enough to be implemented.
+
+Problem: 
+You have three jugs, measuring 12 gallons, 8 gallons, and 3 gallons, and a water faucet. 
+You can fill the jugs from the faucet, or from one another, or pour their contents on the ground. 
+The goal is to measure out (in a jug) exactly one gallon. 
+ 
+*/
