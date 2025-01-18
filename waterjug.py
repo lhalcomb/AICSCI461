f83e2ad846b4d996fc5fb055fe6529b95d427bce
lhalcomb <123402670+lhalcomb@users.noreply.github.com>
2025-01-17 21:50:25 -0600

    waterjug into py not cpp

--- waterjug.py
+++ waterjug.py
@@ -1,3 +1,6 @@
+
+ 
+"""
 /*
 2. Give a complete formulation (state, initial state, actions, etc.) to the following problem. 
 
@@ -7,5 +10,4 @@ Problem:
 You have three jugs, measuring 12 gallons, 8 gallons, and 3 gallons, and a water faucet. 
 You can fill the jugs from the faucet, or from one another, or pour their contents on the ground. 
 The goal is to measure out (in a jug) exactly one gallon. 
- 
-*/
+"""
